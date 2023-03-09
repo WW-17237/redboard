9ea537d94cfdcc4bdb4e9a1c2087ee5dbf646ef1
William Walker <124848849+WW-17237@users.noreply.github.com>
2023-03-09 14:04:14 +1300

    change myLog to fileReadWrite

--- Example1_WritingLog/Example1_WritingLog.ino
+++ Example1_WritingLog/Example1_WritingLog.ino
@@ -1,32 +1,32 @@
 #include <Wire.h>
 #include "SparkFun_Qwiic_OpenLog_Arduino_Library.h"
-OpenLog myLog; // Create instance
+OpenLog fileReadWrite; // Create instance
 String File = "HEVHELLO.txt"; // Name of text file
 
 void setup()
 {
   Wire.begin(); //Initialize I2C
-  myLog.begin(); //Open connection to OpenLog (no pun intended)
+  fileReadWrite.begin(); //Open connection to OpenLog (no pun intended)
 
   Serial.begin(9600); //9600bps is used for debug statements
   
   //Record something to the default log
-  //myLog.println("This goes to the log file");
+  //fileReadWrite.println("This goes to the log file");
   //Serial.println("This goes to the terminal");
 
   // Fetch size of file
-  long sizeOfFile = myLog.size(File);
+  long sizeOfFile = fileReadWrite.size(File);
 
   if (sizeOfFile == -1) // Check if file exists. if size > -1 it exists but may be empty.
   {  
-    myLog.create("HEVHELLO.txt");
-    myLog.println("Welcome to the HEV mark 4 hazardous environment suit");
+    fileReadWrite.create("HEVHELLO.txt");
+    fileReadWrite.println("Welcome to the HEV mark 4 hazardous environment suit");
   } else {
-    myLog.append("HEVHELLO.txt");
-    myLog.println("Injecting Morphine");
+    fileReadWrite.append("HEVHELLO.txt");
+    fileReadWrite.println("Injecting Morphine");
   }
 
-  myLog.syncFile(); // Write to card
+  fileReadWrite.syncFile(); // Write to card
 
   Serial.println();
 }
