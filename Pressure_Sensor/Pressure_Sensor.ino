da4a9df4cf471e4395651c7e6ffc016947506ef2
William Walker <124848849+WW-17237@users.noreply.github.com>
2023-05-18 14:42:18 +1200

    Pressure sensor sketch compiling :)
    Commented
    contains openLog and LPS25HB code

---
+++ Pressure_Sensor/Pressure_Sensor.ino
@@ -0,0 +1,51 @@
+#include <Wire.h>
+#include "SparkFun_Qwiic_OpenLog_Arduino_Library.h"
+#include <SparkFun_LPS25HB_Arduino_Library.h> // Import the pressure sensor library
+LPS25HB pressureSensor; // Create the object for the LPS25HB
+OpenLog logFile;         // Create instance
+String File = "LPS25HB.txt";  // Name of text file
+String Dir = "WW-17237-Dir";
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  logFile.begin();  //Open connection to OpenLog
+  pressureSensor.begin(); // links to I2C port for the sensor
+
+  if (pressureSensor.isConnected() == false) // Check the sensor is present
+  {
+    Serial.println("LPS25HB disconnected. Reset the board to try again.");
+    while (1)
+      ;
+  }
+
+
+  logFile.changeDirectory(".."); //Make sure we're in the root directory
+  logFile.makeDirectory(Dir); // Create directory
+  logFile.changeDirectory(Dir); //Go into this new directory
+  long sizeOfFile = logFile.size(File);
+
+  if (sizeOfFile == -1)  // Check if file exists. if size > -1 it exists but may be empty.
+  { // File didn't exist.
+    Serial.println("File Didn't exist");
+    logFile.append(File);
+    logFile.println("// Begin File //");
+  } else {  // File existed prior to script running
+    Serial.println("File exists appending");
+    logFile.append(File);
+    logFile.println("// New Session //");
+  }
+
+  logFile.syncFile();  // Write to card
+}
+
+void loop()
+{
+  while(pressureSensor.isConnected() == true) {
+    logFile.println("Pressure in hPa: ");
+    logFile.println(pressureSensor.getPressure_hPa()); // Get the pressure reading in hPa
+    logFile.println(", Temperature (degC): ");
+    logFile.println(pressureSensor.getTemperature_degC()); // Get the temperature in degrees C
+    delay(40); // Wait - 40 ms corresponds to the maximum update rate of the sensor (25 Hz)
+  }
+}
