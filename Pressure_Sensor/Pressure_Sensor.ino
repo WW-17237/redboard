e21694bdb78146db69d64c7978d845e36631e867
William Walker <124848849+WW-17237@users.noreply.github.com>
2023-06-07 11:00:19 +1200

    working on csv

--- Pressure_Sensor/Pressure_Sensor.ino
+++ Pressure_Sensor/Pressure_Sensor.ino
@@ -5,14 +5,15 @@
 #include <Servo.h>                                   // import the library required for servos
 
 LPS25HB basicSensor;  // Create the object for the LPS25HB
-ICM_20948_I2C gyro;   // create an gyro object
 Servo chuteMotor;     // create servo object for control
-OpenLog logFile;  // Create instance
+OpenLog logFile;      // Create instance
 ICM_20948_I2C Gyroscope;
-icm_20948_DMP_data_t data;
-String File = "data.csv";  // Name of text file
+icm_20948_DMP_data_t data;  // create gyro object
+String File = "data.csv";   // Name of text file
 String Dir = "WW-17237-Dir";
 float acc_z;
+float maxTemp;
+float minTemp;
 const byte SRVPIN = 9;
 bool chuteDeployed = false;
 
@@ -23,7 +24,7 @@ void setup() {
   logFile.begin();            //Open connection to OpenLog
   basicSensor.begin();        // links to I2C port for the sensor
   chuteMotor.attach(SRVPIN);  // attaches the servo on pin byte (SRVPIN)
-  chuteMotor.write(0); // Reset the servo to 0 degrees
+  chuteMotor.write(0);        // Reset the servo to 0 degrees
   // Check the sensor is present
   if (basicSensor.isConnected() == false) {
     Serial.println("What did you do this time the pressure sensor is disconnected.");
@@ -31,6 +32,8 @@ void setup() {
       ;
   }
 
+  float maxTemp = basicSensor.getTemperature_degC();
+  float minTemp = basicSensor.getTemperature_degC();
 
   logFile.changeDirectory("..");  //Make sure we're in the root directory
   logFile.makeDirectory(Dir);     // Create directory
@@ -63,22 +66,71 @@ void loop() {
     // Don't know how i'll do temp max or min
     logFile.println(", ");
 
-    if ((data.header & DMP_header_bitmap_Accel) > 0)  // Check for change in Accel
-    {
-      float acc_x = (float)data.Raw_Accel.Data.X;  // Extract the raw accelerometer data
-      float acc_y = (float)data.Raw_Accel.Data.Y;
-      acc_z = (float)data.Raw_Accel.Data.Z;
+    float acc_x = (float)data.Raw_Accel.Data.X;  // Extract the raw accelerometer data
+    float acc_y = (float)data.Raw_Accel.Data.Y;
+    acc_z = (float)data.Raw_Accel.Data.Z;
 
-      logFile.print(F("Accel: X:"));
-      logFile.print(acc_x);
-      logFile.print(F(" Y:"));
-      logFile.print(acc_y);
-      logFile.print(F(" Z:"));
-      logFile.println(acc_z);
+    if (maxTemp < basicSensor.getTemperature_degC()) {
+      maxTemp = basicSensor.getTemperature_degC();
+    }
+    if (minTemp > basicSensor.getTemperature_degC()) {
+      minTemp = basicSensor.getTemperature_degC();
     }
 
-    if (acc_z <= 0.2) { // if accelleration is < 0.2 trigger chute
-      chuteOpen(); // Trigger servo method
+
+    // Write sensor data to CSV file
+    logFile.print(basicSensor.getPressure_hPa());  // Get the pressure reading in hPa
+    logFile.print(",");
+    logFile.print(basicSensor.getMaxPressureMillibars());
+    logFile.print(",");
+    logFile.print(basicSensor.getMinPressureMillibars());
+    logFile.print(",");
+    logFile.print(basicSensor.getTemperature_degC());  // Get the temperature in degrees C
+    logFile.print(",");
+    logFile.print(maxTemp);
+    logFile.print(",");
+    logFile.print(minTemp);
+    logFile.print(",");
+    logFile.print(acc_x);
+    logFile.print(",");
+    logFile.print(Gyroscope.getAccelXMax());
+    logFile.print(",");
+    logFile.print(Gyroscope.getAccelXMin());
+    logFile.print(",");
+    logFile.print(acc_y);
+    logFile.print(",");
+    logFile.print(Gyroscope.getAccelYMax());
+    logFile.print(",");
+    logFile.print(Gyroscope.getAccelYMin());
+    logFile.print(",");
+    logFile.print(acc_z);
+    logFile.print(",");
+    logFile.print(Gyroscope.getAccelZMax());
+    logFile.print(",");
+    logFile.print(Gyroscope.getAccelZMin());
+    logFile.print(",");
+    logFile.print(gyroX);
+    logFile.print(",");
+    logFile.print(Gyroscope.getGyroXMax());
+    logFile.print(",");
+    logFile.print(Gyroscope.getGyroXMin());
+    logFile.print(",");
+    logFile.print(gyroY);
+    logFile.print(",");
+    logFile.print(Gyroscope.getGyroYMax());
+    logFile.print(",");
+    logFile.print(Gyroscope.getGyroYMin());
+    logFile.print(",");
+    logFile.print(gyroZ);
+    logFile.print(",");
+    logFile.print(Gyroscope.getGyroZMax());
+    logFile.print(",");
+    logFile.println(Gyroscope.getGyroZMin());
+
+
+
+    if (acc_z <= 0.2) {  // if accelleration is < 0.2 trigger chute
+      chuteOpen();       // Trigger servo method
     }
 
     delay(40);           // Wait - 40 ms corresponds to the maximum update rate of the sensor (25 Hz)
@@ -88,7 +140,7 @@ void loop() {
 
 void chuteOpen() {  // Method for opening the chute
   if (!chuteDeployed) {
-    chuteMotor.write(90);
     chuteDeployed = true;
+    chuteMotor.write(90);
   }
 }
