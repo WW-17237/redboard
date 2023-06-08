2b7838b21ba911e567de6801dc9cda65e101d12f
William Walker <124848849+WW-17237@users.noreply.github.com>
2023-06-09 09:56:35 +1200

    New method
    Created a separate method for saving data to openlog
    commented out thines in the log that i don't know how to get yet

--- Pressure_Sensor/Pressure_Sensor.ino
+++ Pressure_Sensor/Pressure_Sensor.ino
@@ -12,8 +12,12 @@ icm_20948_DMP_data_t data;  // create gyro object
 String File = "data.csv";   // Name of text file
 String Dir = "WW-17237-Dir";
 float acc_z;
+float acc_y;
+float acc_x;
 float maxTemp;
 float minTemp;
+float maxPress;
+float minPress;
 const byte SRVPIN = 9;
 bool chuteDeployed = false;
 
@@ -28,12 +32,13 @@ void setup() {
   // Check the sensor is present
   if (basicSensor.isConnected() == false) {
     Serial.println("What did you do this time the pressure sensor is disconnected.");
-    while (1)
-      ;
+    while (1);
   }
 
-  float maxTemp = basicSensor.getTemperature_degC();
-  float minTemp = basicSensor.getTemperature_degC();
+  maxTemp = basicSensor.getTemperature_degC();
+  minTemp = basicSensor.getTemperature_degC();
+  maxPress = basicSensor.getPressure_hPa();
+  minPress = basicSensor.getPressure_hPa();
 
   logFile.changeDirectory("..");  //Make sure we're in the root directory
   logFile.makeDirectory(Dir);     // Create directory
@@ -46,7 +51,8 @@ void setup() {
     logFile.append(File);
     logFile.println("Time(ms), Pressure, Pressure Max, Pressure Min, Temperature, Temperature Max, Temperature Min, AccelerometerX, AccelerometerX Max, AccelerometerX Min, AccelerometerY, AccelerometerY Max, AccelerometerY Min, AccelerometerZ, AccelerometerZ Max, AccelerometerZ Min, GyroX, GyroX Max, GyroX Min, GyroY, GyroY Max, GyroY Min, GyroZ, GyroZ Max, GyroZ Min");
   } else {  // File existed prior to script running
-    Serial.println("File exists appending");
+    Serial.println("File exists wiping");
+    logFile.removeFile(File);
     logFile.append(File);
     logFile.println("Time(ms), Pressure, Pressure Max, Pressure Min, Temperature, Temperature Max, Temperature Min, AccelerometerX, AccelerometerX Max, AccelerometerX Min, AccelerometerY, AccelerometerY Max, AccelerometerY Min, AccelerometerZ, AccelerometerZ Max, AccelerometerZ Min, GyroX, GyroX Max, GyroX Min, GyroY, GyroY Max, GyroY Min, GyroZ, GyroZ Max, GyroZ Min");
   }
@@ -60,85 +66,89 @@ void loop() {
   //HAN NOTES just to keep the code easier to read the order of things in the loop
   Gyroscope.readDMPdataFromFIFO(&data);  // Read a frame from the data
   while (basicSensor.isConnected() == true) { //HAN NOTES what about the advanced IMU sensor? or do you have two of these if statements
-
-    logFile.println(basicSensor.getPressure_hPa());  // Get the pressure reading in hPa
-    logFile.println(", ");
-    // Don't know how i'll do pressure max or min
-    logFile.println(basicSensor.getTemperature_degC());  // Get the temperature in degrees C
-    // Don't know how i'll do temp max or min
-    logFile.println(", ");
-
-    float acc_x = (float)data.Raw_Accel.Data.X;  // Extract the raw accelerometer data
-    float acc_y = (float)data.Raw_Accel.Data.Y;
+    acc_x = (float)data.Raw_Accel.Data.X;  // Extract the raw accelerometer data
+    acc_y = (float)data.Raw_Accel.Data.Y;
     acc_z = (float)data.Raw_Accel.Data.Z;
 
+    // Get max temp if the current temp is higher update max temp
     if (maxTemp < basicSensor.getTemperature_degC()) {
       maxTemp = basicSensor.getTemperature_degC();
     }
+    // Get min temp if current temp is lower then update min temp
     if (minTemp > basicSensor.getTemperature_degC()) {
       minTemp = basicSensor.getTemperature_degC();
     }
+    // Get max pressure by storing whatever the highest pressure
+    // is between launch and current time
+    if (maxPress < basicSensor.getPressure_hPa()) {
+      maxPress = basicSensor.getPressure_hPa();
+    }
+    // get the min pressure by storing the lowest recorded pressure
+    // between launch and current time
+    if (minPress > basicSensor.getPressure_hPa()) {
+      minPress = basicSensor.getPressure_hPa();
+    }
 
-
-    // Write sensor data to CSV file
-    logFile.print(basicSensor.getPressure_hPa());  // Get the pressure reading in hPa
-    logFile.print(",");
-    logFile.print(basicSensor.getMaxPressureMillibars());
-    logFile.print(",");
-    logFile.print(basicSensor.getMinPressureMillibars());
-    logFile.print(",");
-    logFile.print(basicSensor.getTemperature_degC());  // Get the temperature in degrees C
-    logFile.print(",");
-    logFile.print(maxTemp);
-    logFile.print(",");
-    logFile.print(minTemp);
-    logFile.print(",");
-    logFile.print(acc_x);
-    logFile.print(",");
-    logFile.print(Gyroscope.getAccelXMax());
-    logFile.print(",");
-    logFile.print(Gyroscope.getAccelXMin());
-    logFile.print(",");
-    logFile.print(acc_y);
-    logFile.print(",");
-    logFile.print(Gyroscope.getAccelYMax());
-    logFile.print(",");
-    logFile.print(Gyroscope.getAccelYMin());
-    logFile.print(",");
-    logFile.print(acc_z);
-    logFile.print(",");
-    logFile.print(Gyroscope.getAccelZMax());
-    logFile.print(",");
-    logFile.print(Gyroscope.getAccelZMin());
-    logFile.print(",");
-    logFile.print(gyroX);
-    logFile.print(",");
-    logFile.print(Gyroscope.getGyroXMax());
-    logFile.print(",");
-    logFile.print(Gyroscope.getGyroXMin());
-    logFile.print(",");
-    logFile.print(gyroY);
-    logFile.print(",");
-    logFile.print(Gyroscope.getGyroYMax());
-    logFile.print(",");
-    logFile.print(Gyroscope.getGyroYMin());
-    logFile.print(",");
-    logFile.print(gyroZ);
-    logFile.print(",");
-    logFile.print(Gyroscope.getGyroZMax());
-    logFile.print(",");
-    logFile.println(Gyroscope.getGyroZMin());
-
-
+    storeData();
 
     if (acc_z <= 0.2) {  // if accelleration is < 0.2 trigger chute
       chuteOpen();       // Trigger servo method
     }
 
-    delay(40);           // Wait - 40 ms corresponds to the maximum update rate of the sensor (25 Hz)
-   //HAN NOTES I would put this immediately after the last logFile line of code 
+    delay(40); // Wait 40 ms the maximum update rate of the sensor (25 Hz)
+   }
+}
+
+void storeData() {
+  // Write sensor data to CSV file
+    logFile.println(basicSensor.getPressure_hPa());  // Get the pressure reading in hPa
+    logFile.print(",");
+    logFile.print(maxPress); // trip highest pressure
+    logFile.print(",");
+    logFile.print(minPress); // trip lowest pressure
+    logFile.print(",");
+    logFile.print(basicSensor.getTemperature_degC());  // Get the temperature in degrees C
+    logFile.print(",");
+    logFile.print(maxTemp); // trip highest temp
+    logFile.print(",");
+    logFile.print(minTemp); // trip lowest temp
+    logFile.print(",");
+    logFile.print(acc_x);
+    logFile.print(",");
+    //logFile.print(Gyroscope.getAccelXMax());
+    logFile.print(",");
+    //logFile.print(Gyroscope.getAccelXMin());
+    logFile.print(",");
+    logFile.print(acc_y);
+    logFile.print(",");
+    //logFile.print(Gyroscope.getAccelYMax());
+    logFile.print(",");
+    //logFile.print(Gyroscope.getAccelYMin());
+    logFile.print(",");
+    logFile.print(acc_z);
+    logFile.print(",");
+    //logFile.print(Gyroscope.getAccelZMax());
+    logFile.print(",");
+    //logFile.print(Gyroscope.getAccelZMin());
+    logFile.print(",");
+    //logFile.print(gyroX);
+    logFile.print(",");
+    //logFile.print(Gyroscope.getGyroXMax());
+    logFile.print(",");
+    //logFile.print(Gyroscope.getGyroXMin());
+    logFile.print(",");
+    //logFile.print(gyroY);
+    logFile.print(",");
+    //logFile.print(Gyroscope.getGyroYMax());
+    logFile.print(",");
+    //logFile.print(Gyroscope.getGyroYMin());
+    logFile.print(",");
+    //logFile.print(gyroZ);
+    logFile.print(",");
+    //logFile.print(Gyroscope.getGyroZMax());
+    logFile.print(",");
+    //logFile.print(Gyroscope.getGyroZMin());
     logFile.syncFile();  // Write to card
-  }
 }
 
 void chuteOpen() {  // Method for opening the chute
@@ -147,3 +157,4 @@ void chuteOpen() {  // Method for opening the chute
     chuteMotor.write(90);
   }
 }
+
