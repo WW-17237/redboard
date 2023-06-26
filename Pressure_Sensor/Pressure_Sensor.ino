a4ffe0aff44f7ae0f316b61f2cedd8fba10d33e3
William Walker <124848849+WW-17237@users.noreply.github.com>
2023-06-27 11:05:33 +1200

    Update
    - Cleaned up loop method
    - Shifted accelerometer and gyro data gathering to its own method
    - added check to see if the imu sensor is connected

--- Pressure_Sensor/Pressure_Sensor.ino
+++ Pressure_Sensor/Pressure_Sensor.ino
@@ -32,9 +32,10 @@ void setup() {
   // Check the sensor is present
   if (basicSensor.isConnected() == false) {
     Serial.println("What did you do this time the pressure sensor is disconnected.");
-    while (1);
+    while (1)
+      ;
   }
-//HAN NOTES this will set the max and min to the same value that will then cause issues in you if statements lower down?
+  //HAN NOTES this will set the max and min to the same value that will then cause issues in you if statements lower down?
   maxTemp = basicSensor.getTemperature_degC();
   minTemp = basicSensor.getTemperature_degC();
   maxPress = basicSensor.getPressure_hPa();
@@ -64,91 +65,98 @@ void setup() {
 void loop() {
   //HAN NOTES I would suggest taking this chunk of code that deals with the sensors and having it in is own method like your servo
   //HAN NOTES just to keep the code easier to read the order of things in the loop
-  Gyroscope.readDMPdataFromFIFO(&data);  // Read a frame from the data
-  while (basicSensor.isConnected() == true) { //HAN NOTES what about the advanced IMU sensor? or do you have two of these if statements
-    acc_x = (float)data.Raw_Accel.Data.X;  // Extract the raw accelerometer data
-    acc_y = (float)data.Raw_Accel.Data.Y;
-    acc_z = (float)data.Raw_Accel.Data.Z;
-
-    // Get max temp if the current temp is higher update max temp
-    if (maxTemp < basicSensor.getTemperature_degC()) {
-      maxTemp = basicSensor.getTemperature_degC();
-    }
-    // Get min temp if current temp is lower then update min temp
-    if (minTemp > basicSensor.getTemperature_degC()) {
-      minTemp = basicSensor.getTemperature_degC();
-    }
-    // Get max pressure by storing whatever the highest pressure
-    // is between launch and current time
-    if (maxPress < basicSensor.getPressure_hPa()) {
-      maxPress = basicSensor.getPressure_hPa();
-    }
-    // get the min pressure by storing the lowest recorded pressure
-    // between launch and current time
-    if (minPress > basicSensor.getPressure_hPa()) {
-      minPress = basicSensor.getPressure_hPa();
-    }
-
+  while (basicSensor.isConnected() && Gyroscope.isConnected()) {  //HAN NOTES what about the advanced IMU sensor? or do you have two of these if statements
+    gyroData();
+    cacheMinMax();
     storeData();
 
     if (acc_z <= 0.2) {  // if accelleration is < 0.2 trigger chute
       chuteOpen();       // Trigger servo method
     }
 
-    delay(40); // Wait 40 ms the maximum update rate of the sensor (25 Hz)
-   }
+    delay(40);  // Wait 40 ms the maximum update rate of the sensor (25 Hz)
+  }
+}
+
+// Grab data from the Gyro
+void gyroData() {
+  Gyroscope.readDMPdataFromFIFO(&data);  // Read a frame from the data
+  acc_x = (float)data.Raw_Accel.Data.X;  // Extract the raw accelerometer data
+  acc_y = (float)data.Raw_Accel.Data.Y;
+  acc_z = (float)data.Raw_Accel.Data.Z;
+}
+
+void cacheMinMax() {
+  // Get max temp if the current temp is higher update max temp
+  if (maxTemp < basicSensor.getTemperature_degC()) {
+    maxTemp = basicSensor.getTemperature_degC();
+  }
+  // Get min temp if current temp is lower then update min temp
+  if (minTemp > basicSensor.getTemperature_degC()) {
+    minTemp = basicSensor.getTemperature_degC();
+  }
+  // Get max pressure by storing whatever the highest pressure
+  // is between launch and current time
+  if (maxPress < basicSensor.getPressure_hPa()) {
+    maxPress = basicSensor.getPressure_hPa();
+  }
+  // get the min pressure by storing the lowest recorded pressure
+  // between launch and current time
+  if (minPress > basicSensor.getPressure_hPa()) {
+    minPress = basicSensor.getPressure_hPa();
+  }
 }
 
 void storeData() {
   // Write sensor data to CSV file
-    logFile.println(basicSensor.getPressure_hPa());  // Get the pressure reading in hPa
-    logFile.print(",");
-    logFile.print(maxPress); // trip highest pressure
-    logFile.print(",");
-    logFile.print(minPress); // trip lowest pressure
-    logFile.print(",");
-    logFile.print(basicSensor.getTemperature_degC());  // Get the temperature in degrees C
-    logFile.print(",");
-    logFile.print(maxTemp); // trip highest temp
-    logFile.print(",");
-    logFile.print(minTemp); // trip lowest temp
-    logFile.print(",");
-    logFile.print(acc_x);
-    logFile.print(",");
-    //logFile.print(Gyroscope.getAccelXMax());
-    logFile.print(",");
-    //logFile.print(Gyroscope.getAccelXMin());
-    logFile.print(",");
-    logFile.print(acc_y);
-    logFile.print(",");
-    //logFile.print(Gyroscope.getAccelYMax());
-    logFile.print(",");
-    //logFile.print(Gyroscope.getAccelYMin());
-    logFile.print(",");
-    logFile.print(acc_z);
-    logFile.print(",");
-    //logFile.print(Gyroscope.getAccelZMax());
-    logFile.print(",");
-    //logFile.print(Gyroscope.getAccelZMin());
-    logFile.print(",");
-    //logFile.print(gyroX);
-    logFile.print(",");
-    //logFile.print(Gyroscope.getGyroXMax());
-    logFile.print(",");
-    //logFile.print(Gyroscope.getGyroXMin());
-    logFile.print(",");
-    //logFile.print(gyroY);
-    logFile.print(",");
-    //logFile.print(Gyroscope.getGyroYMax());
-    logFile.print(",");
-    //logFile.print(Gyroscope.getGyroYMin());
-    logFile.print(",");
-    //logFile.print(gyroZ);
-    logFile.print(",");
-    //logFile.print(Gyroscope.getGyroZMax());
-    logFile.print(",");
-    //logFile.print(Gyroscope.getGyroZMin());
-    logFile.syncFile();  // Write to card
+  logFile.println(basicSensor.getPressure_hPa());  // Get the pressure reading in hPa
+  logFile.print(",");
+  logFile.print(maxPress);  // trip highest pressure
+  logFile.print(",");
+  logFile.print(minPress);  // trip lowest pressure
+  logFile.print(",");
+  logFile.print(basicSensor.getTemperature_degC());  // Get the temperature in degrees C
+  logFile.print(",");
+  logFile.print(maxTemp);  // trip highest temp
+  logFile.print(",");
+  logFile.print(minTemp);  // trip lowest temp
+  logFile.print(",");
+  logFile.print(acc_x);
+  logFile.print(",");
+  //logFile.print(Gyroscope.getAccelXMax());
+  logFile.print(",");
+  //logFile.print(Gyroscope.getAccelXMin());
+  logFile.print(",");
+  logFile.print(acc_y);
+  logFile.print(",");
+  //logFile.print(Gyroscope.getAccelYMax());
+  logFile.print(",");
+  //logFile.print(Gyroscope.getAccelYMin());
+  logFile.print(",");
+  logFile.print(acc_z);
+  logFile.print(",");
+  //logFile.print(Gyroscope.getAccelZMax());
+  logFile.print(",");
+  //logFile.print(Gyroscope.getAccelZMin());
+  logFile.print(",");
+  //logFile.print(gyroX);
+  logFile.print(",");
+  //logFile.print(Gyroscope.getGyroXMax());
+  logFile.print(",");
+  //logFile.print(Gyroscope.getGyroXMin());
+  logFile.print(",");
+  //logFile.print(gyroY);
+  logFile.print(",");
+  //logFile.print(Gyroscope.getGyroYMax());
+  logFile.print(",");
+  //logFile.print(Gyroscope.getGyroYMin());
+  logFile.print(",");
+  //logFile.print(gyroZ);
+  logFile.print(",");
+  //logFile.print(Gyroscope.getGyroZMax());
+  logFile.print(",");
+  //logFile.print(Gyroscope.getGyroZMin());
+  logFile.syncFile();  // Write to card
 }
 
 void chuteOpen() {  // Method for opening the chute
@@ -157,4 +165,3 @@ void chuteOpen() {  // Method for opening the chute
     chuteMotor.write(90);
   }
 }
-
